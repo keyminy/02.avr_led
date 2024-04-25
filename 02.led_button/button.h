ea8cf1e25ac34cbc9166eda7e9d82d3ccd558c66
keyminy <imanaifs@gmail.com>
2024-04-25 15:31:17 +0900

    버튼으로 제어 완성

--- 02.led_button/button.h
+++ 02.led_button/button.h
@@ -15,20 +15,25 @@
 
 #define LED_DDR		DDRA // 이렇게 하는 이유는 LED_DDR에서 바꿔주면 compiler가 알아서 변경해준다
 #define LED_PORT	PORTA
-#define BUTTON_DDR  DDRD;
-#define BUTTON_PIN  PIND; // PORTD를 read하는 register, 전압5v를 읽으면 1, 0v를 읽으면 0
+#define BUTTON_DDR  DDRD
+#define BUTTON_PIN  PIND // PORTD를 read하는 register, 전압5v를 읽으면 1, 0v를 읽으면 0
 
-#define BUTTON1 4 // PORTD.4
-#define BUTTON2 5 // PORTD.5
-#define BUTTON3 6 // PORTD.6
-#define BUTTON4 7 // PORTD.7
+#define BUTTON1_PIN 4 // PORTD.4
+#define BUTTON2_PIN 5 // PORTD.5
+#define BUTTON3_PIN 6 // PORTD.6
+#define BUTTON4_PIN 7 // PORTD.7
+
+/* 버튼 번호 */
+#define BUTTON1 0
+#define BUTTON2 1 
+#define BUTTON3 2 
+#define BUTTON4 3 
+
+#define BUTTON_PRESS 1 //버튼을 누르면 high active-high
+#define BUTTON_RELEASE 0 //버튼을 떼면 LOW
+#define BUTTON_NUMBER 4 // 버튼 갯수는 4개다
 
 #endif //  BUTTON_H
 
 // portA는 write모드
 // portD는 button이 연결되었음
-
-
-
-
-#endif /* BUTTON_H_ */
--- 02.led_button/main.c
+++ 02.led_button/main.c
@@ -8,9 +8,12 @@
 #include "button.h"
 extern void init_button(void); // init_button함수는 button.c에 있다 "extern키워드"
 // extern : 해당 함수가 다른 파일에 있음을 컴파일러에게 알려준다.
+extern int get_button(int button_pin, int button_number); // button.c에 해당 함수가 있으므로 알려준다
 
 int main(void)
 {
+	int button1_state = 0; // 초기상태는 버튼을 off상태로 출발합니다.
+
 	LED_DDR = 0xff; // LED가 출력 모드 이므로 all 1로 설정을 한다.
 	LED_PORT = 0x00; // LED를 모두 off한다.
 
@@ -19,6 +22,17 @@ int main(void)
     /* Replace with your application code */
     while (1) 
     {
+		// 버튼1은 portd4에 연결됨
+		// (1) 버튼1을 1번 눌렀다 때면 led전체를 on시키고
+		// (2) 버튼1을 1번 눌렀다 떼면 led전체를 off
+		//지금 get_button(BUTTON1_PIN,1)은 1로 하드코딩됨
+		if(get_button(BUTTON1_PIN,BUTTON1)){ // 버튼을 1번 눌렀다 떼면 반전시킴
+			button1_state = !button1_state;
+			if(button1_state){
+				LED_PORT = 0xff;
+			}else {
+				LED_PORT = 0x00;
+			}
+		}
     }
 }
-
